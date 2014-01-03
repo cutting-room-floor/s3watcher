8cc98c25ab33b6a3e908291a46c23f09651ef840
Mick Thompson <dthompson@gmail.com>
2014-01-03 10:48:48 -0800

    refactoring, style fixes, config example

--- index.js
+++ index.js
@@ -6,104 +6,71 @@ var config, s3;
 
 
 watch.config = function(c){
-  config = c;
-  AWS.config.update({accessKeyId: c.awsKey, secretAccessKey: c.awsSecret});
-  s3 = new AWS.S3();
-}
+    config = c;
+    AWS.config.update({accessKeyId: c.awsKey, secretAccessKey: c.awsSecret});
+    s3 = new AWS.S3();
+};
+
+// Files from cloudfront look like this:
 // {bucket-name}.s3.amazonaws.com/{optional-prefix/}{distribution-ID}.{YYYY}-{MM}-{DD}-{HH}.{unique-ID}.gz
 
-// E1MSP5URRDWXN6.2013-04-14-00.62RFS2Yg.gz
-
 var baseMarker = process.env.BASE_MARKER;
-
-
 var seenKeys = [];
 
 function checkForNew(){
 
-  var hours = last24hours();
-  hours.forEach(function(h){
-    checkForNewByTime(h);
-  });
+    var hours = last24hours();
+    hours.forEach(function(h){
+        checkForNewByTime(h);
+    });
 }
 
+var months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+var days = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16',
+            '17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
+
 function last24hours(){
-  
-  var hours = Array.apply(null, Array(24));
-  var now = new Date();
+    var hours = Array.apply(null, Array(24));
+    var now = new Date();
+    hours = hours.map(function(_, i){
+        var d = new Date(now-36e5*(i+1));
+        return d.getFullYear()+"-"+months[d.getMonth()]+"-"+days[d.getDay()]+"-"+d.getHours();
+    });
 
-  var months = Array.apply(null, Array(12)).map(function(_, i){
-    if((i+1+"").length === 1) return "0"+(i+1);
-    else return i+1+"";
-  })
-
-  var days = Array.apply(null, Array(31)).map(function(_, i){
-    if((i+1+"").length === 1) return "0"+(i+1);
-    else return i+1+"";
-  })
-
-
-  hours = hours.map(function(_, i){
-    var d = new Date(now-36e5*(i+1));
-    return d.getFullYear()+"-"+months[d.getMonth()]+"-"+days[d.getDay()]+"-"+d.getHours();
-  });
-
-  return hours;
+    return hours;
 }
 
 function checkForNewByTime(date){
- 
-  var opts = {"Marker":baseMarker+date,
-              "Prefix":baseMarker+date,
-              "Bucket":config.bucket}
+    
+    var opts = {"Marker":baseMarker+date,
+                "Prefix":baseMarker+date,
+                "Bucket":config.bucket};
 
-  //console.log(opts);
+    s3.listObjects(opts, function(err, data){
+        if(err) console.log("Error", err);
 
-  s3.listObjects(opts, function(err, data){
-    if(err) console.log("Error", err);
-    //console.log(data);
+        data.Contents.forEach(function(c){
 
-    data.Contents.forEach(function(c){
-
-      if(seenKeys.indexOf(c.Key) === -1){  
-        seenKeys.push(c.Key);
-        watch.push(c.Key+"\n");
-      }
+            if(seenKeys.indexOf(c.Key) === -1){  
+                seenKeys.push(c.Key);
+                watch.push(c.Key+"\n");
+            }
+        });
     });
-  });
 }
 
-
-/*setTimeout(function(){
-  watch.push('beep 3 ', 'utf8');
-  watch.push('boop\n', 'utf8');
-
-  watch.push(null);
-
-}, 2000)
-*/
-
-
 var started = false;
-
-
 watch._read = function(){
+    if(!started) start();
 
-
-  if(!started) start();
-
-  started = true;
-  return true;
+    started = true;
+    return true;
 };
 
-
 function start(){
-
-  var check = function(){
-    checkForNew();
-    setTimeout(check, 5000)
-  }
-
-  check();
-
+    var check = function(){
+        checkForNew();
+        setTimeout(check, 5000);
+    };
+    check();
 }
--- test/test.js
+++ test/test.js
@@ -1,12 +1,11 @@
 var assert = require("assert");
-
-var s3watcher = require('../index.js')
-
+var s3watcher = require('../index.js');
 
 
+s3watcher.config({awsKey:process.env.AWS_KEY, 
+                  awsSecret:process.env.AWS_SECRET, 
+                  bucket:process.env.BUCKET});
 
 
 s3watcher.pipe(process.stdout);
-
-
 
