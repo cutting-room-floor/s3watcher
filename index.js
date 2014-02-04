0af5a2d434256928bb417a35c1d7021dd72276d4
Mick Thompson <dthompson@gmail.com>
2014-02-04 15:21:18 -0800

    check if the date hasnt been set

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ watch.checkForNew = function(cb){
         resultDate = results.reduce(function(a, b){
             return (a > b ? a : b);
         });
+        console.log(resultDate, lastDate)
         if(resultDate > lastDate){
             lastDate = resultDate;
             watch.setLastDate(lastDate, function(e){
@@ -84,6 +85,8 @@ function checkForNewByTime(date,  cb){
         s3.listObjects(opts, function(err, data){
             if(err) return cb(err);
 
+            console.log("in ", opts.Prefix, " there are ", data.Contents.length);
+
             data.Contents.forEach(function(c){
                 if(c.LastModified > lastDate){
                     watch.push(c.Key+'\n');
@@ -123,7 +126,6 @@ watch.setLastDate = function(date, cb){
     var opts = {Bucket:config.bucket,
                 Key:config.prefixDir+'.s3watcher',
                 Body: (+date).toString()};
-
     s3.putObject(opts, function(err, resp){
         cb(err);
     });
@@ -138,6 +140,7 @@ watch.getLastDate = function(cb){
 
         try{
             var lastDate = new Date(parseInt(resp.Body.toString()));
+            console.log("got date: ", lastDate);
             cb(err, lastDate);
         }catch(e){
             cb(e);
@@ -158,8 +161,13 @@ watch._read = function(){
 function start(){
     var check = function(){
         watch.getLastDate(function(err, date){
-            if(date === undefined)
-                date = +(new Date(1));
+
+            console.log("last Date:", date)
+
+            if(date === undefined){
+                date = (new Date());
+                watch.setLastDate(date, function(){});
+            }
 
             lastDate = date;
             watch.checkForNew(function(err){
