a55353881c0e16674953a1dced48b7e5d1220d2c
Will White <will@mapbox.com>
2014-02-14 15:53:44 -0500

    Always back off marker.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var path = require('path');
 var util = require('util');
 var AWS = require ('aws-sdk');
 var Readable = require('stream').Readable;
-var debug = require('debug')('s3watcher');
+var debug = require('debug');
+var log = debug('s3watcher');
+var verbose = debug('s3watcher:verbose');
 var queue = require('queue-async');
 var LRU = require('lru-cache');
 
@@ -19,10 +21,13 @@ watch.config = function(c) {
     config.processed = path.join(config.state, 'emitted');
     AWS.config.update({accessKeyId: c.awsKey, secretAccessKey: c.awsSecret});
     s3 = new AWS.S3();
+
+    log('saving marker at %s', config.watchkey);
+    log('saving processed at %s', config.processed);
 };
 
 watch.saveState = function(marker, callback){
-    debug('saving marker %s to s3://%s/%s', marker, config.bucket, config.watchkey);
+    log('saving marker to S3');
     s3.putObject({
         Bucket: config.bucket,
         Key: config.watchkey,
@@ -31,7 +36,7 @@ watch.saveState = function(marker, callback){
 };
 
 watch.loadState = function(callback) {
-    debug('loading state from s3://%s/%s', config.bucket, config.watchkey);
+    log('loading marker from S3');
     var opts = {
         Bucket: config.bucket,
         Key: config.watchkey
@@ -40,7 +45,7 @@ watch.loadState = function(callback) {
     s3.getObject(opts, function(err, resp) {
         if (err) {
             if (err.code === 'NoSuchKey') {
-                debug('state file does not exist; starting at the top');
+                log('state file does not exist; starting at the top');
 
                 return s3.listObjects({
                     Prefix: config.prefix,
@@ -59,8 +64,8 @@ watch.loadState = function(callback) {
         }
 
         try {
-            var marker = resp.Body.toString();
-            debug('parsed marker %s', marker);
+            var marker = resp.Body.toString().trim();
+            log('parsed marker %s', marker);
             callback(null, marker);
         } catch(e) {
             callback(e);
@@ -77,16 +82,18 @@ watch._read = function(){
 };
 
 function start() {
-    debug('starting');
+    log('starting');
     (function check() {
-        debug('checking');
+        log('checking');
 
         watch.loadState(function(err, marker) {
             if (err) return watch.emit('error', err);
 
+            marker = dateToKey(new Date(keyToDate(marker) - 864e5), config.prefix);
+
             scan(marker, function(err) {
                 if (err) return watch.emit('error', err);
-                debug('waiting for %d ms', config.timeout);
+                log('waiting for %d ms', config.timeout);
                 setTimeout(check, config.timeout);
             });
         });
@@ -95,31 +102,29 @@ function start() {
 
 // Scan a bucket starting at the given marker and call emit() on each key.
 function scan(marker, callback) {
-    debug('scanning at %s for keys', marker);
-
     (function fetch(opts){
-        debug('list objects starting with marker %s', opts.Marker);
+        log('list objects starting with marker %s', opts.Marker);
         s3.listObjects(opts, function(err, data){
             if (err) return callback(err);
             if (!data.Contents.length) return callback();
 
             var q = queue(10);
+            var emitted = 0;
             _(data.Contents).each(function(obj) {
                 if (obj.Key.indexOf('.s3watcher') !== -1) return
-                q.defer(emit, obj.Key);
+                q.defer(function(callback) {
+                    emit(obj.Key, function(err, e) {
+                        if (e) emitted++;
+                        callback(err, e);
+                    });
+                });
             });
             q.awaitAll(function(err) {
                 if (err) return callback(err);
 
-                if (+keyToDate(marker) > Date.now() - 864e5) {
-                    var oldmarker = marker;
-                    marker = dateToKey(new Date(Date.now() - 864e5), config.prefix);
-                    debug('marker %s less than 24 hour old; using %s instead', oldmarker, marker);
-                } else {
-                    marker = _(data.Contents).last().Key;
-                }
+                log('emitted %d of %d keys', emitted, data.Contents.length);
 
-                watch.saveState(marker, function(err) {
+                watch.saveState(_(data.Contents).last().Key, function(err) {
                     if (err) return callback(err);
                     if (data.IsTruncated) {
                         opts.Marker = _(data.Contents).last().Key;
@@ -138,9 +143,9 @@ function scan(marker, callback) {
 }
 
 var emitted = LRU({
-    max: 24000,
+    max: 25000,
     dispose: function() {
-        debug('disposed cache object');
+        verbose('disposed cache object');
     }
 });
 
@@ -168,9 +173,9 @@ function emit(key, callback) {
         // Emit the key and record it was emitted in S3 and the local cache.
         s3.putObject(opts, function(err) {
             if (err) return callback(err);
-            watch.push(key + '\n');
             emitted.set(key, true);
-            callback();
+            watch.push(key + '\n');
+            callback(null, true);
         });
     });
 }
